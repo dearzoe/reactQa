5672916a88402fdd35c265dd059d64949cf08673
dearzoe <2421113739@qq.com>
2016-11-19 23:22:03 +0800

    安装就绪

---
+++ react_qa/app/js/main.js
@@ -0,0 +1,7 @@
+var ReactDOM = require("react-dom");
+var QuestionApp = require("./com/questionApp.js");
+
+var comp = ReactDOM.render(
+    <QuestionApp/>,
+    document.getElementById("app")
+);
--- react_qa/gulpfile.js
+++ react_qa/gulpfile.js
@@ -1,2 +1,35 @@
 var gulp = require("gulp"),
-    connect= require("gulp-connect")
+    connect= require("gulp-connect"),
+    browserify= require("gulp-browserify"),
+    concat=require("gulp-concat"),
+    port=process.env.port || 5000;
+gulp.task("browserify",function(){
+    gulp.src('./app/js/main.js')
+        .pipe(browserify({
+            transform:'reactify'
+        }))
+        .pipe(gulp.dest('./dist/js'))
+});
+//livereload
+gulp.task("server", function () {
+    connect.server({
+        root:'./',//服务器的根目录
+        port:port,//服务器的地址，没有此配置是8080
+        livereload:true
+    })
+});
+gulp.task("copy-js", function () {
+    gulp.src('./dist/**/*.js')
+        .pipe(connect.reload())
+});
+gulp.task("copy-html", function () {
+    gulp.src('./app/**/*.html')
+        .pipe(connect.reload())
+});
+gulp.task("watch", function () {
+    gulp.watch("./dist/**/*.js",["copy-js"]);
+    gulp.watch("./app/**/*.html",["copy-html"]);
+    gulp.watch("./app/**/*.js",["browserify"]);
+})
+gulp.task("default",["browserify"]);
+gulp.task("serve",["browserify","server","watch"]);
